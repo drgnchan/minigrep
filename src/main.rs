cfee0d6c02c9cb4f53545768883d7aef2060ec05
drgnchan <40224023+drgnchan@users.noreply.github.com>
2023-01-30 10:47:01 +0800

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let config = parse_config(&args);
+    println!("query is {}, filename is {}", config.query, config.filename)
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    Config{
+        query: args[1].clone(),
+        filename: args[2].clone(),
+    }
 }
